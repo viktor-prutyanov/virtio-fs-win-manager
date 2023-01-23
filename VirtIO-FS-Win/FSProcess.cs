a7558799daec1286233ab394d8dfd9780028baf0
Viktor Prutyanov <viktor.prutyanov@phystech.edu>
2023-01-22 16:05:40 -0800

    Add VirtIO-FS process list

---
+++ VirtIO-FS-Win/FSProcess.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtIO_FS_Win
+{
+    public class FSProcess
+    {
+        public readonly string mountPoint;
+        public readonly string debugLogFile;
+        public readonly string debugFlags;
+        public readonly string tag;
+        public readonly bool caseInsensitive;
+
+        public readonly string args;
+
+        private readonly Process process = new Process();
+        private static string MakeStrArg(string arg, string key)
+        {
+            return !string.IsNullOrEmpty(arg) ? $"{key} {arg}" : "";
+        }
+
+        private static string MakeBoolArg(bool arg, string key)
+        {
+            return arg ? key : "";
+        }
+
+        public FSProcess(string binaryPath, string mountPoint, string debugLogFile, string debugFlags, string tag, bool caseInsensitive)
+        {
+            this.mountPoint = mountPoint;
+            this.debugLogFile = debugLogFile;
+            this.debugFlags = debugFlags;
+            this.tag = tag;
+            this.caseInsensitive = caseInsensitive;
+
+            string mountPointArg = MakeStrArg(mountPoint, "-m");
+            string debugLogFileArg = MakeStrArg(debugLogFile, "-D");
+            string debugFlagsArg = MakeStrArg(debugFlags, "-d");
+            string tagArg = MakeStrArg(tag, "-t");
+            string caseInsensitiveArg = MakeBoolArg(caseInsensitive, "-i");
+
+            args = $"{mountPointArg} {debugLogFileArg} {debugFlagsArg} {tagArg} {caseInsensitiveArg}";
+
+            process.StartInfo.FileName = binaryPath;
+            process.StartInfo.Arguments = args;
+        }
+
+        public bool Start()
+        {
+            return process.Start();
+        }
+
+        public void Kill()
+        {
+            process.Kill();
+        }
+
+        override public string ToString()
+        {
+            return $"virtiofs {args}";
+        }
+    }
+}
--- VirtIO-FS-Win/MainWindow.xaml.cs
+++ VirtIO-FS-Win/MainWindow.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Diagnostics;
 using Microsoft.Win32;
+using System.Collections.ObjectModel;
 
 namespace VirtIO_FS_Win
 {
@@ -22,11 +23,12 @@ namespace VirtIO_FS_Win
     /// </summary>
     public partial class MainWindow : Window
     {
-        private readonly Process process = new Process();
-
+        private readonly ObservableCollection<FSProcess> fsProcs = new ObservableCollection<FSProcess>();
         public MainWindow()
         {
             InitializeComponent();
+
+            FSProcListBox.ItemsSource = fsProcs;
         }
 
         private static string GetWinFspVersion()
@@ -41,28 +43,15 @@ namespace VirtIO_FS_Win
             }
         }
 
-        private static string MakeStrArg(string arg, string key)
-        {
-            return !string.IsNullOrEmpty(arg) ? $"{key} {arg}" : "";
-        }
-
-        private static string MakeBoolArg(bool arg, string key)
-        {
-            return arg ? key : "";
-        }
-
         private void Button_Click_Start(object sender, RoutedEventArgs e)
         {
-            string MountPointArg = MakeStrArg(MountPointTB.Text, "-m");
-            string DebugLogFileArg = MakeStrArg(DebugLogFileTB.Text, "-D");
-            string DebugFlagsArg = MakeStrArg(DebugFlagsTB.Text, "-d");
-            string TagArg = MakeStrArg(TagTB.Text, "-t");
-            string CaseInsensitiveArg = MakeBoolArg(CaseInsensitiveCB.IsChecked ?? false, "-i");
+            FSProcess fsProcess = new FSProcess(BinaryPathTB.Text, MountPointTB.Text, DebugLogFileTB.Text, DebugFlagsTB.Text, TagTB.Text, CaseInsensitiveCB.IsChecked ?? false);
 
-            process.StartInfo.FileName = PathTB.Text;
-            process.StartInfo.Arguments = $"{MountPointArg} {DebugLogFileArg} {DebugFlagsArg} {TagArg} {CaseInsensitiveArg}";
-
-            process.Start();
+            if (fsProcess.Start())
+            {
+                fsProcs.Add(fsProcess);
+                FSProcListBox.SelectedIndex = -1;
+            }
         }
 
         private void Button_Click_Select(object sender, RoutedEventArgs e)
@@ -71,27 +60,47 @@ namespace VirtIO_FS_Win
 
             if (dialog.ShowDialog() == true)
             {
-                PathTB.Text = dialog.FileName;
+                BinaryPathTB.Text = dialog.FileName;
             }
         }
 
         private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
         {
-            Properties.Settings.Default.ProgramPath = PathTB.Text;
+            Properties.Settings.Default.ProgramPath = BinaryPathTB.Text;
             Properties.Settings.Default.Save();
         }
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            PathTB.Text = Properties.Settings.Default.ProgramPath;
+            BinaryPathTB.Text = Properties.Settings.Default.ProgramPath;
             WinFspVersionTB.Text = GetWinFspVersion();
         }
 
         private void Button_Click_Stop(object sender, RoutedEventArgs e)
         {
-            if (!process.HasExited)
+            if (fsProcs.Count() != 0)
             {
-                process.Kill();
+                int idx = FSProcListBox.SelectedIndex;
+
+                if (idx != -1)
+                {
+                    fsProcs[idx].Kill();
+                    fsProcs.RemoveAt(idx);
+                }
+            }
+        }
+
+        private void FSProcListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            int idx = FSProcListBox.SelectedIndex;
+
+            if (idx != -1)
+            {
+                MountPointTB.Text = fsProcs[idx].mountPoint;
+                DebugLogFileTB.Text = fsProcs[idx].debugLogFile;
+                DebugFlagsTB.Text = fsProcs[idx].debugFlags;
+                TagTB.Text = fsProcs[idx].tag;
+                CaseInsensitiveCB.IsChecked = fsProcs[idx].caseInsensitive;
             }
         }
     }
